435df27e74948b6fbfc2a8e0d7d2ca9ecd44e5ea
kelle <kellerhydle@gmail.com>
2025-04-25 08:48:46 -0600

    Updates
    -implemented basic rust to python communication

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+
+
+#[pyfunction]
+fn test() -> PyResult<()> {
+    println!("Hello from rust!");
+    Ok(())
+}
+
+#[pymodule]
+fn ibrowse(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(test, py)?)?;
+    Ok(())
 }
